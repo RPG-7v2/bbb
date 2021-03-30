b6ce1c6e86e6b1696e1ac9a5845d3db244316d1b
RPG-7v2 <81508175+RPG-7v2@users.noreply.github.com>
2021-03-30 10:04:45 +0400

    Add files via upload

--- Flat.java
+++ Flat.java
@@ -1,6 +1,27 @@
 public class Flat {
     private byte roomNum;
     private double roomArea;
-    private 
+
+    public byte getRoomNum() {
+        return roomNum;
+    }
+
+    public double getRoomArea() {
+        return roomArea;
+    }
+
+    public void setRoomNum(byte roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public void setRoomArea(double roomArea) {
+        this.roomArea = roomArea;
+    }
+
+    public Flat(byte roomsCount, double roomsArea ){
+        this.roomNum = roomsCount;
+        this.roomArea = roomsArea;
+    }
+
 
 }
